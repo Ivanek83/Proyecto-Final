b55de861b6806a0f7f3adc2fb9615adc21c98a3b
Ivan <ivanpablok@hotmail.com>
2023-06-11 11:46:27 -0300

    Carga de bootstrap y es navegable dentro de django.

--- Proyecto/apps/home/views.py
+++ Proyecto/apps/home/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+
+def projects(request):
+    return render(request, 'home/projects.html')
+
+
+def resume(request):
+    return render(request, 'home/resume.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
